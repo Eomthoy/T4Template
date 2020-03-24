4dfacada6ef5a47a23166f101ff7ee2c1f203538
Eom <2512410481@qq.com>
2020-03-24 20:12:28 +0800

    优化

--- Eomthoy.T4.DataBaseTest/Program.cs
+++ Eomthoy.T4.DataBaseTest/Program.cs
@@ -14,29 +14,29 @@ namespace Eomthoy.T4.DataBaseTest
         {
             #region SqlServer
 
-            //SqlHelper.connString = @"Server=1TVFGO8OATR3Y7G\SQLEXPRESS;DataBase=MyDB;User Id=sa;Password=123;";
-            //string sql = "select * from sys.tables where name like '%%' ";
-            //List<Tables> tables = SqlHelper.GetAll<Tables>(sql, CommandType.Text);
-            //string newSql = "SELECT [columns].*, [descriptions].[value] as [description], [type].[name] as [type] FROM sys.tables as [tables] INNER JOIN sys.columns as [columns] on [columns].[object_id]=[tables].[object_id] LEFT JOIN sys.extended_properties [descriptions] on [descriptions].[major_id]=[tables].[object_id] and [descriptions].[minor_id]=[columns].[column_id] LEFT JOIN sys.types as [type] on [type].[user_type_id]=[columns].[user_type_id]";
-            //List<Columns> columns = SqlHelper.GetAll<Columns>(newSql, CommandType.Text);
-            //foreach (Tables table in tables)
-            //{
-            //    table.Columns = columns.Where(x => x.object_id == table.object_id).ToList();
-            //    MapType(table.Columns);
-            //}
+            SqlHelper.connString = @"Server=.;DataBase=RecordDB;User Id=sa;Password=123456;";
+            string sql = "select * from sys.tables where name like '%%' and name in ('Conn_PCase_BodyCheck', 'AllergyHistory')";
+            List<Tables> tables = SqlHelper.GetAll<Tables>(sql, CommandType.Text);
+            string newSql = "SELECT [columns].*, [descriptions].[value] as [description], [type].[name] as [type] FROM sys.tables as [tables] INNER JOIN sys.columns as [columns] on [columns].[object_id]=[tables].[object_id] LEFT JOIN sys.extended_properties [descriptions] on [descriptions].[major_id]=[tables].[object_id] and [descriptions].[minor_id]=[columns].[column_id] LEFT JOIN sys.types as [type] on [type].[user_type_id]=[columns].[user_type_id]";
+            List<Columns> columns = SqlHelper.GetAll<Columns>(newSql, CommandType.Text);
+            foreach (Tables table in tables)
+            {
+                table.Columns = columns.Where(x => x.object_id == table.object_id).ToList();
+                MapType(table.Columns);
+            }
             #endregion
 
             #region MySQL
-            MySqlHelper.connString = @"Server=localhost;DataBase=mydb;User Id=root;Password=123456;";
-            string sql = "SELECT table_name name, table_comment description FROM information_schema.TABLES WHERE table_schema = ( SELECT DATABASE ( ) )  and table_name like '%%' ";
-            List<Tables> tables = MySqlHelper.GetAll<Tables>(sql, CommandType.Text);
-            string newSql = "SELECT table_name tableName, column_name name,	data_type type,	column_comment description,	(CASE is_nullable WHEN 'YES' THEN 1 ELSE 0 END) as is_nullable FROM information_schema.COLUMNS WHERE table_schema = (SELECT DATABASE() ) ORDER BY ordinal_position";
-            List<Columns> columns = MySqlHelper.GetAll<Columns>(newSql, CommandType.Text);
-            foreach (Tables table in tables)
-            {
-                table.Columns = columns.Where(x => x.tableName == table.name).ToList();
-                MapType(table.Columns);
-            }
+            //MySqlHelper.connString = @"Server=localhost;DataBase=mydb;User Id=root;Password=123456;";
+            //string sql = "SELECT table_name name, table_comment description FROM information_schema.TABLES WHERE table_schema = ( SELECT DATABASE ( ) )  and table_name like '%%' ";
+            //List<Tables> tables = MySqlHelper.GetAll<Tables>(sql, CommandType.Text);
+            //string newSql = "SELECT table_name tableName, column_name name,	data_type type,	column_comment description,	(CASE is_nullable WHEN 'YES' THEN 1 ELSE 0 END) as is_nullable FROM information_schema.COLUMNS WHERE table_schema = (SELECT DATABASE() ) ORDER BY ordinal_position";
+            //List<Columns> columns = MySqlHelper.GetAll<Columns>(newSql, CommandType.Text);
+            //foreach (Tables table in tables)
+            //{
+            //    table.Columns = columns.Where(x => x.tableName == table.name).ToList();
+            //    MapType(table.Columns);
+            //}
             #endregion
 
 
